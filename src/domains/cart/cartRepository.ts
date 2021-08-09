2dac473435674a5d8a255eeeaa45e71d072cc186
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2021-08-09 09:42:03 +0700

    feat: cart data layer

--- src/domains/cart/cartRepository.ts
+++ src/domains/cart/cartRepository.ts
@@ -1,8 +1,9 @@
 import { Cart } from './cartEntity';
+import { CartProps } from './cartInterface';
 import { Either, DataError } from '../../shared';
 
 export interface CartRepository {
-  // create(cart: Cart): Promise<Either<DataError,Cart>>;
+  create(cart: CartProps): Promise<Either<DataError,Cart>>;
   update(cart: Cart): Promise<Either<DataError,Cart>>;
   getById(id: string): Promise<Either<DataError,Cart>>;
 }
